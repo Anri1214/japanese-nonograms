2e3700a4e1992fff7463eb894ecac093d056ee56
Anri1214 <Anri1214.K@gmail.com>
2018-12-03 21:58:32 +0200

    Updates

---
+++ precache-manifest.be7cc5cb472793d0d15585460670e3c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "302f71283f0503aaeeab",
+    "url": "/japanese-nonograms/static/js/runtime~main.302f7128.js"
+  },
+  {
+    "revision": "1ce1cafe1f7faab39199",
+    "url": "/japanese-nonograms/static/js/main.1ce1cafe.chunk.js"
+  },
+  {
+    "revision": "fb63cc0325cfe8568025",
+    "url": "/japanese-nonograms/static/js/1.fb63cc03.chunk.js"
+  },
+  {
+    "revision": "1ce1cafe1f7faab39199",
+    "url": "/japanese-nonograms/static/css/main.c398651a.chunk.css"
+  },
+  {
+    "revision": "0fc73fa97453335ec103044f51177ae5",
+    "url": "/japanese-nonograms/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0bdacd994d2ed11073937834c79cd07.js"
+  "/japanese-nonograms/precache-manifest.be7cc5cb472793d0d15585460670e3c4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/japanese-nonograms/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
