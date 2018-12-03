037939c58cd2ca040911309f73d8e7e686529640
Anri1214 <Anri1214.K@gmail.com>
2018-12-03 22:43:37 +0200

    Add main redirect

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -22,6 +22,7 @@ class Main extends Component {
           <Settings />
           <Switch>
             <Route exact path="/" render={ () => <Redirect to="/game" /> }/>
+            <Route exact path="/japanese-nonograms" render={ () => <Redirect to="/game" /> }/>
             <Route exact path="/about" component={ About } />
             <Route exact path="/help" component={ Help } />
             <Route exact path="/game" component={ Game } />
