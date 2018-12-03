9480f90af733a13bf4164a0deaab1bed8a5a6bb3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 22:28:14 +0000

    Deploy Anri1214/japanese-nonograms to github.com/Anri1214/japanese-nonograms.git:gh-pages

--- precache-manifest.27dc9cb67994d77bf655a57593388037.js
+++ precache-manifest.27dc9cb67994d77bf655a57593388037.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/japanese-nonograms/static/js/runtime~main.302f7128.js"
   },
   {
-    "revision": "7da709397ef54b3740e9",
-    "url": "/japanese-nonograms/static/js/main.7da70939.chunk.js"
+    "revision": "d870d2d85b46f1fd5b3c",
+    "url": "/japanese-nonograms/static/js/main.d870d2d8.chunk.js"
   },
   {
     "revision": "fb63cc0325cfe8568025",
     "url": "/japanese-nonograms/static/js/1.fb63cc03.chunk.js"
   },
   {
-    "revision": "7da709397ef54b3740e9",
+    "revision": "d870d2d85b46f1fd5b3c",
     "url": "/japanese-nonograms/static/css/main.dbcabaec.chunk.css"
   },
   {
-    "revision": "f667de2326d4f166648e8200dbc0b5b3",
+    "revision": "29610247974da6c36cec26c60e1a2f7a",
     "url": "/japanese-nonograms/index.html"
   }
 ];
